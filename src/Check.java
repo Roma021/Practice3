907e8f66e097270e862349d18cbe7ead2fa67e91
Roma021 <dtmogh@gmail.com>
2019-06-18 16:48:40 -0400

    Practiceagain

--- src/Check.java
+++ src/Check.java
@@ -38,14 +38,55 @@ public class Check {
       //  else {
        //     System.out.println("Your username or password was invalid!");
       //  }
-        while (true) {
-            System.out.println("Do you enjoy programming!");
-            System.out.println("Do you want to continue? ");
-            String continue1 = reader.nextLine();
-            if (continue1.equals("No")) {
-                break;
-            }
-        }
-        System.out.println(" Thank you, see you later! ");
+       // while (true) {
+      //      System.out.println("Do you enjoy programming!");
+        //    System.out.println("Do you want to continue? ");
+          //  String continue1 = reader.nextLine();
+            //if (continue1.equals("No")) {
+              //  break;
+       //     }
+     //   }
+    //    System.out.println(" Thank you, see you later! ");
+
+        //System.out.println("welcome to the calculator");
+
+       // while (true) {
+       //     System.out.print("Enter a command (sum, difference, quit): ");
+       //     String command = reader.nextLine();
+       //     if (command.equals("quit")) {
+         //       break;
+       //     }
+
+         //   System.out.print("Enter a number: ");
+        //    int first = Integer.parseInt(reader.nextLine());
+        //    System.out.println("Enter another number: ");
+        //    int second = Integer.parseInt(reader.nextLine());
+
+         //   if (command.equals("sum") ) {
+         //       int sum = first + second;
+         //       System.out.println( "The sum of the numbers is: " + sum );
+         //   } else if (command.equals("difference")) {
+         //       int difference = first - second;
+         //       System.out.println("The difference of the numbers is: " + difference);
+         //   } else {
+         //       System.out.println("Unknown command");
+          //  }
+
+       // }
+     //   System.out.println("Thanks, bye!");
+
+       // while (true) {
+       //     System.out.println("Type the password: ");
+       //     String pass1 = reader.nextLine();
+       //     if (pass1.equals("Roma021")) {
+       //         break;
+       //     }
+       //     else {
+        //        System.out.println("Wrong! please try again.");
+        //    }
+   //     }
+   //     System.out.println("You are now logged in to the system!");
+
+
     }
 }
